0cdd2d38b8e5f5559b06d8d25d63748810c010ad
Abir Qasem <abir.qasem@gmail.com>
2017-02-09 16:54:09 -0500

    descriptive readme and node/gas
    Added a meaningful readme.
    the code should work in both GAS and Node.

--- number_currency_extractor.js
+++ number_currency_extractor.js
@@ -1,6 +1,6 @@
 function get_numbers (str_array, extract) {
   var numbers_data = {all_numbers:[], currency:""};
-  
+
   for (s in str_array) {
     var potential_number = str_array[s];
     var currency_regex = /\$|[\u00A3,\u00A5, \u20A1]/;// add the unicode of the currency symbol you want
@@ -18,8 +18,8 @@ function get_numbers (str_array, extract) {
       }
     }
   }
-      
-    
+
+
   return numbers_data;
 
 
@@ -30,7 +30,7 @@ function extract_dot_system (number_string) {
     var extracted_num;
     if (number_string.indexOf(',') <0) {
       extracted_num = Number (number_string)
-    } 
+    }
     else {
       number_string = number_string.replace(',','');
       extracted_num = Number (number_string)
@@ -41,13 +41,13 @@ function extract_dot_system (number_string) {
 
 
 function extract_comma_system (number_string) {
-    
+
   number_string =translate(number_string)
   //Logger.log(number_string)
   var extracted_num;
     if (number_string.indexOf(',') <0) {
       extracted_num = Number (number_string)
-    } 
+    }
     else {
       number_string = number_string.replace(',','');
       extracted_num = Number (number_string)
@@ -62,26 +62,26 @@ function extract_comma_system (number_string) {
 
 
 function translate (string) {
-  
+
   //string = '123.456.789,01'
   var new_str =""
-  
+
   for (var i=0; i<string.length;i++) {
     var change='';
     if (string[i]==',')
       change = '.'
     else if (string [i] =='.')
       change =','
-    
+
     if (change.length >0)
       new_str = new_str+ change
      else
        new_str = new_str+string[i];
   }
-  
+
   return new_str;
-  
-  
+
+
 }
 
 
@@ -93,15 +93,25 @@ function translate (string) {
 
 
 function test_numbers () {
-  
-  
+
+
   var str_array = ['£123', '51,123.5', '$34.5561']
   var spanish = ['¥125.502,6']
-  
   //var currency_regex = /\$|[\u20A1]/;// add the unicode of the currency symbol you want
   //Logger.log(spanish[0].search(currency_regex));
-  
- Logger.log(get_numbers(spanish, extract_comma_system));
-  
-  
+
+ try {
+   Logger.log(get_numbers(spanish, extract_comma_system))
+ } catch (e) {
+
+   console.log(get_numbers(spanish, extract_comma_system));
+
+ } finally {
+   //
+ }
+
+
 }
+
+
+test_numbers()
